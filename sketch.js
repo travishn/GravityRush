581bf51654efa4dc5ace2cb507f8f94d44611253
Travis <travishn@uci.edu>
2018-06-21 16:56:12 -0700

    Fix start button and bg music

--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
 let player;
-let firstGame = false;
+var started = false;
 let isOver = false;
 let bgX = 0;
 let terrain = [];
@@ -31,10 +31,21 @@ function preload() {
 // CREDIT TO EFFECTS GRINDER FOR SFX
 function setup() {
 	cnv = createCanvas(1080, 720);
+	cnv.parent('canvas-container');
 	centerCanvas();
+
 	resetGame();
 	startBtn = createButton('Start Game');
-	startBtn.mousePressed(resetGame);
+	startBtn.mousePressed(start);
+
+	noLoop();
+}
+
+function start() {
+	started = true;
+	song.jump(0);
+	song.setVolume(0.5);
+	loop();
 }
 
 function windowResized() {
@@ -48,36 +59,41 @@ function centerCanvas() {
 }
 
 function draw() {
-	background(0);
-	image(bgImg, bgX, 0, bgImg.width, height);
-	bgX -= terrain[terrain.length-1].speed * 0.8;
-
-	if (bgX <= -bgImg.width + width) {
-		image(bgImg, bgX + bgImg.width, 0, bgImg.width, height);
-		if (bgX <= -bgImg.width) {
-			bgX = 0;
-		}
-	}  
+	if (started) {
+		background(0);
+		image(bgImg, bgX, 0, bgImg.width, height);
+		bgX -= terrain[terrain.length-1].speed * 0.8;
 	
-	for (let i = terrain.length - 1; i >= 0; i--) {
-		terrain[i].show();
-		terrain[i].update();
-		setLimit(terrain[i]);
-		deleteTerrain(terrain[i]);
-
-		if (player.isDead(terrain[i])) {
-			gameOver();
-			player.showFinalScore();
-		} else {
-			player.showScore();
+		if (bgX <= -bgImg.width + width) {
+			image(bgImg, bgX + bgImg.width, 0, bgImg.width, height);
+			if (bgX <= -bgImg.width) {
+				bgX = 0;
+			}
+		}  
+		
+		for (let i = terrain.length - 1; i >= 0; i--) {
+			terrain[i].show();
+			terrain[i].update();
+			setLimit(terrain[i]);
+			deleteTerrain(terrain[i]);
+	
+			if (player.isDead(terrain[i])) {
+				gameOver();
+				player.showFinalScore();
+			} else {
+				player.showScore();
+			}
 		}
+		
+		option = options[Math.floor(Math.random()*options.length)];
+		if (!isOver && inRange(terrain[terrain.length-1])) {
+			const nextTerrain = new Terrain(option);
+			terrain.push(nextTerrain);
+		}
+	
+		player.update();
+		player.show();
 	}
-	
-	option = options[Math.floor(Math.random()*options.length)];
-	if (!isOver && inRange(terrain[terrain.length-1])) terrain.push(new Terrain(option));
-
-	player.update();
-	player.show();
 }
 
 function deleteTerrain(targetTerrain) {
@@ -88,7 +104,7 @@ function setLimit(targetTerrain) {
 	if (targetTerrain.hits(player) && targetTerrain.flipped) {
 		player.topLimit = targetTerrain.terrainHeight;
 	} else if (targetTerrain.hits(player) && targetTerrain.flipped === false) {
-		player.bottomLimit = height - (targetTerrain.terrainHeight) - player.playerHeight;
+		player.bottomLimit = height - (targetTerrain.terrainHeight) - player.playerHeight + 10;
 	} else {
 		player.topLimit = -100;
 		player.bottomLimit = height + this.playerHeight;
@@ -117,8 +133,11 @@ function resetGame() {
 	firstTerrain.x = 400;
 	firstTerrain.terrainWidth = 1100;
 	terrain.push(firstTerrain);
-	song.jump(0);
-	song.setVolume(0.5);
+
+	if (started) {
+		song.jump(0);
+		song.setVolume(0.5);
+	}
 }
 
 function inRange(currentTerrain) {
